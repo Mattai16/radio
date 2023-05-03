c54188baef04a627423bb3bf8233e9dd7a3fdeb9
MauricioAburtoL <xlr.2000@hotmail.com>
2023-05-03 08:03:08 -0600

    carpetas organizadas

---
+++ src/vistas/Perfiles.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Menu from "../components/Menu";
+import Show from "../components/Show";
+
+const Perfiles = () =>{
+    return(
+        
+        <div>
+            <Menu />
+            <Show/>
+        </div>  
+    )
+}
+
+
+export default Perfiles
---
+++ src/vistas/Tienda.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Edit from "../components/Edit";
+import Menu from "../components/Menu";
+
+const Tienda = () =>{
+    return(
+        
+        <div>
+            
+            <Menu />
+            <h1>Hola tienda</h1>
+            
+        </div>  
+    )
+}
+
+
+export default Tienda
