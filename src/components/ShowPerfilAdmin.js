e78c96865ad6cbf700ef571d5a4a7b8481e42175
Mattai16 <111460191+Mattai16@users.noreply.github.com>
2023-05-23 23:19:48 -0600

    .
    Cambios

--- src/components/ShowPerfilAdmin.js
+++ src/components/ShowPerfilAdmin.js
@@ -1,113 +1,85 @@
-import React, { useState, useEffect } from 'react'
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
-import { Button } from 'mdb-react-ui-kit';
-import { Link } from 'react-router-dom'
-import { collection, getDocs, getDoc, deleteDoc, doc } from 'firebase/firestore'
-import { db } from './firebaseConfig/firebase'
+import React, { useState, useEffect } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
+import { Link } from 'react-router-dom';
+import { collection, getDocs, getDoc, deleteDoc, doc } from 'firebase/firestore';
+import { db } from './firebaseConfig/firebase';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
-import Header from './Header';
-const MySawl = withReactContent(Swal)
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import Menu from './Menu';
 
-const ShowPerilAdmin = () => {
+const MySwal = withReactContent(Swal);
 
-    const [perfiles, setPerfiles] = useState([])
+const ShowPerfilAdmin = () => {
+    const [perfiles, setPerfiles] = useState([]);
 
-    const perfilesCollection = collection(db, "perfiles")
+    const perfilesCollection = collection(db, 'perfiles');
 
     const getPerfiles = async () => {
-        const data = await getDocs(perfilesCollection)
-        /* console.log(data) */
-        setPerfiles(
-            data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-        )
-        console.log(perfiles)
-    }
+        const data = await getDocs(perfilesCollection);
+        setPerfiles(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
     const confirmDelete = (id) => {
-        MySawl.fire({
-            title: '¿Estas seguro de eliminar el perfil?',
-            text: "No podras recuperar los datos",
+        MySwal.fire({
+            title: '¿Estás seguro de eliminar el perfil?',
+            text: 'No podrás recuperar los datos',
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Sí, eliminalo'
-          }).then((result) => {
+            confirmButtonText: 'Sí, elímnalo',
+        }).then((result) => {
             if (result.isConfirmed) {
-                deletePerfil(id)
-              Swal.fire(
-                'Deleted!',
-                'Ha sido borrado.',
-                'Completado'
-              )
+                deletePerfil(id);
+                MySwal.fire('Deleted!', 'Ha sido borrado.', 'success');
             }
-          })
-    }
+        });
+    };
 
     const deletePerfil = async (id) => {
-        const perfilesDoc = doc(db, "perfiles", id)
-        await deleteDoc(perfilesDoc)
-        getPerfiles()
-    }
+        const perfilesDoc = doc(db, 'perfiles', id);
+        await deleteDoc(perfilesDoc);
+        getPerfiles();
+    };
 
     useEffect(() => {
-        getPerfiles()
-    }, [])
+        getPerfiles();
+    }, []);
 
     return (
-        <>
-
-        <Header/>
-
-        
+        <div>
+            <Menu />
             <div className="vh-100">
                 <MDBContainer>
-
-                    { perfiles.map( (perfil) =>(
-                                <MDBRow className="justify-content-center" key={perfil.id}>
-                                <MDBCol md="9" lg="7" xl="5" className="mt-5">
-                                    <MDBCard style={{ borderRadius: '0px' }}>
-                                        <MDBCardBody className="p-4">
-                                            <div className="d-flex text-black">
-                                                <div className="flex-shrink-0">
-                                                    <MDBCardImage
-                                                        style={{ width: '80px', borderRadius: '10px' }}
-                                                        src='https://imgs.search.brave.com/GJ9XRZT2i6S2jnjcceZxYj5U2ZSZc9WkvL2bF1hTYj8/rs:fit:980:980:1/g:ce/aHR0cHM6Ly93ZWJz/dG9ja3Jldmlldy5u/ZXQvaW1hZ2VzL3By/b2ZpbGUtaWNvbi1w/bmctOS5wbmc'
-                                                        alt='Generic placeholder image'
-                                                        fluid />
-                                                </div>
-                                                <div className="flex-grow-1 ms-3">
-                                                    <h3> {perfil.nombre}</h3>
-                                                    <p>{perfil.papel}</p>
-                                                    <div className="d-flex justify-content-start rounded-3 p-2 mb-2"
-                                                        style={{ backgroundColor: '#efefef' }}>
-        
-                                                        <div>
-                                                            {perfil.desc}
-                                                        </div>
-                                                    </div>
-                                                    <div className="d-flex pt-2">
-                                                        <Link to={`/edit/${perfil.id}`} outline className="btn btn-outline-secondary me-1 flex-grow-1">Editar</Link>
-                                                        <button onClick={()=> confirmDelete(perfil.id)} className="btn btn-dark flex-grow-1">Eliminar</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </MDBCardBody>
-                                    </MDBCard>
-                                </MDBCol>
-                            </MDBRow>
-                    ))}
-
-                <Link to={'/create'} className="btn btn-dark flex-grow-1">Crear</Link>
+                    <MDBRow className="row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                        {perfiles.map((perfil) => (
+                            <MDBCol key={perfil.id}>
+                                <MDBCard className="h-100" style={{ margin: '10px', padding: '10 10px' }}>
+                                    <MDBCardImage
+                                        src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyZmlsfGVufDB8fDB8fHww&w=1000&q=80"
+                                        alt="Generic placeholder image"
+                                        fluid
+                                    />
+                                    <MDBCardBody>
+                                        <MDBCardTitle>{perfil.nombre}</MDBCardTitle>
+                                        <MDBCardText>{perfil.papel}</MDBCardText>
+                                        <MDBCardText>{perfil.desc}</MDBCardText>
+                                        <div className="d-flex justify-content-between">
+                                            <Link to={`/edit/${perfil.id}`} outline className="btn btn-outline-secondary">Editar</Link>
+                                            <button onClick={() => confirmDelete(perfil.id)} className="btn btn-dark">Eliminar</button>
+                                        </div>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </MDBCol>
+                        ))}
+                    </MDBRow>
+                    
                 </MDBContainer>
-                
+                <Link to={'/create'} className="btn btn-dark flex-grow-1">Crear</Link>
             </div>
+        </div>
+    );
+};
 
-
-        </>
-    )
-}
-
-export default ShowPerilAdmin
+export default ShowPerfilAdmin;
--- src/components/ShowProgramaAdmin.js
+++ src/components/ShowProgramaAdmin.js
@@ -1,111 +1,88 @@
-import React, { useState, useEffect } from 'react'
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
-import { Button } from 'mdb-react-ui-kit';
-import { Link } from 'react-router-dom'
-import { collection, getDocs, getDoc, deleteDoc, doc } from 'firebase/firestore'
-import { db } from './firebaseConfig/firebase'
+import React, { useState, useEffect } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
+import { Link } from 'react-router-dom';
+import { collection, getDocs, getDoc, deleteDoc, doc } from 'firebase/firestore';
+import { db } from './firebaseConfig/firebase';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 import Header from './Header';
-const MySawl = withReactContent(Swal)
+import Menu from './Menu';
+
+const MySwal = withReactContent(Swal);
 
 const ShowProgramaAdmin = () => {
 
-    const [programa, setPrograma] = useState([])
+    const [programas, setProgramas] = useState([]);
 
-    const programaCollection = collection(db, "programas")
+    const programasCollection = collection(db, 'programas');
 
-    const getPrograma = async () => {
-        const data = await getDocs(programaCollection)
-        /* console.log(data) */
-        setPrograma(
-            data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-        )
-        console.log(programa)
-    }
+    const getProgramas = async () => {
+        const data = await getDocs(programasCollection);
+        setProgramas(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
     const confirmDelete = (id) => {
-        MySawl.fire({
-            title: '¿Estas seguro de eliminar el programa?',
-            text: "No podras recuperar los datos",
+        MySwal.fire({
+            title: '¿Estás seguro de eliminar el Programa?',
+            text: 'No podrás recuperar los datos',
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Sí, eliminalo'
-          }).then((result) => {
+            confirmButtonText: 'Sí, elímnalo',
+        }).then((result) => {
             if (result.isConfirmed) {
-                deletePrograma(id)
-              Swal.fire(
-                'Deleted!',
-                'Ha sido borrado.',
-                'Completado'
-              )
+                deletePrograma(id);
+                MySwal.fire('Deleted!', 'Ha sido borrado.', 'success');
             }
-          })
-    }
+        });
+    };
 
     const deletePrograma = async (id) => {
-        const programaDoc = doc(db, "programas", id)
-        await deleteDoc(programaDoc)
-        getPrograma()
-    }
+        const programasDoc = doc(db, 'programas', id);
+        await deleteDoc(programasDoc);
+        getProgramas();
+    };
 
     useEffect(() => {
-        getPrograma()
-    }, [])
+        getProgramas();
+    }, []);
 
     return (
-        <>
 
-        <Header/>
-
-            <div className="vh-100" style={{ backgroundColor: '#E7F6F2' }}>
+        <div>
+            <Menu />
+            <div className="vh-100">
                 <MDBContainer>
-
-                    { programa.map( (programa) =>(
-                                <MDBRow className="justify-content-center" key={programa.id}>
-                                <MDBCol md="9" lg="7" xl="5" className="mt-5">
-                                    <MDBCard style={{ borderRadius: '0px' }}>
-                                        <MDBCardBody className="p-4">
-                                            <div className="d-flex text-black">
-                                                <div className="flex-shrink-0">
-                                                    <MDBCardImage
-                                                        style={{ width: '80px', borderRadius: '10px' }}
-                                                        src='https://imgs.search.brave.com/GJ9XRZT2i6S2jnjcceZxYj5U2ZSZc9WkvL2bF1hTYj8/rs:fit:980:980:1/g:ce/aHR0cHM6Ly93ZWJz/dG9ja3Jldmlldy5u/ZXQvaW1hZ2VzL3By/b2ZpbGUtaWNvbi1w/bmctOS5wbmc'
-                                                        alt='Generic placeholder image'
-                                                        fluid />
-                                                </div>
-                                                <div className="flex-grow-1 ms-3">
-                                                    <h3> {programa.titulo}</h3>
-                                                    <p>{programa.desc}</p>
-                                                    <div className="d-flex justify-content-start rounded-3 p-2 mb-2"
-                                                        style={{ backgroundColor: '#efefef' }}>
-        
-                                                        <div>
-                                                            {programa.horario}
-                                                        </div>
-                                                    </div>
-                                                    <div className="d-flex pt-2">
-                                                        <Link to={`/editprograma/${programa.id}`} outline className="btn btn-outline-secondary me-1 flex-grow-1">Editar</Link>
-                                                        <button onClick={()=> confirmDelete(programa.id)} className="btn btn-dark flex-grow-1">Eliminar</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </MDBCardBody>
-                                    </MDBCard>
-                                </MDBCol>
-                            </MDBRow>
-                    ))}
-
-                <Link to={'/createprograma'} className="btn btn-dark flex-grow-1">Crear</Link>
+                    <MDBRow className="row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                        {programas.map((programa) => (
+                            <MDBCol key={programa.id}>
+                                <MDBCard className="h-100" style={{ margin: '10px', padding: '10 10px' }}>
+                                    <MDBCardImage
+                                        src={programa.imagen}
+                                        alt="Generic placeholder image"
+                                        fluid
+                                    />
+                                    <MDBCardBody>
+                                        <MDBCardTitle>{programa.titulo}</MDBCardTitle>
+                                        <MDBCardText>{programa.desc}</MDBCardText>
+                                        <MDBCardText>{programa.horario}</MDBCardText>
+                                        <div className="d-flex justify-content-between">
+                                            <Link to={`/edit/${programa.id}`} outline className="btn btn-outline-secondary">Editar</Link>
+                                            <button onClick={() => confirmDelete(programa.id)} className="btn btn-dark">Eliminar</button>
+                                        </div>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </MDBCol>
+                        ))}
+                    </MDBRow>
+                    
                 </MDBContainer>
+                <Link to={'/create'} className="btn btn-dark flex-grow-1">Crear</Link>
             </div>
+        </div>
+    );
+};
 
-
-        </>
-    )
-}
-
-export default ShowProgramaAdmin
+export default ShowProgramaAdmin;
