9f649dbfa4425fd91ebb6788f78d45bad1d6a368
MauricioAburtoL <xlr.2000@hotmail.com>
2023-05-23 21:51:11 -0600

    Carrusel listo

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,19 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
-import { db } from './firebaseConfig/firebase'
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './firebaseConfig/firebase';
 
 export default function Carrusel() {
-  const [programa, setPrograma] = useState([])
+  const [programa, setPrograma] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const getPrograma = async () => {
-      const programaCollection = collection(db, 'programas')
-      const data = await getDocs(programaCollection)
-      setPrograma(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-    }
+      const programaCollection = collection(db, 'programas');
+      const data = await getDocs(programaCollection);
+      setPrograma(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-    getPrograma()
-  }, [])
+    getPrograma();
+  }, []);
+
+  const handlePrev = () => {
+    const newIndex = currentIndex === 0 ? programa.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const handleNext = () => {
+    const newIndex = currentIndex === programa.length - 1 ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
 
   return (
     <>
@@ -24,9 +35,9 @@ export default function Carrusel() {
               key={index}
               type="button"
               data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to={index}
-              className={index === 0 ? 'active' : ''}
-              aria-current={index === 0 ? 'true' : 'false'}
+              data-bs-slide-to={index.toString()}
+              className={index === currentIndex ? 'active' : ''}
+              aria-current={index === currentIndex ? 'true' : 'false'}
               aria-label={`Slide ${index + 1}`}
             ></button>
           ))}
@@ -35,7 +46,7 @@ export default function Carrusel() {
           {programa.map((programaItem, index) => (
             <div
               key={programaItem.id}
-              className={`carousel-item ${index === 0 ? 'active' : ''}`}
+              className={`carousel-item ${index === currentIndex ? 'active' : ''}`}
             >
               <img
                 src={programaItem.imagen}
@@ -54,6 +65,7 @@ export default function Carrusel() {
           type="button"
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide="prev"
+          onClick={handlePrev}
         >
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
@@ -63,11 +75,12 @@ export default function Carrusel() {
           type="button"
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide="next"
+          onClick={handleNext}
         >
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
       </div>
     </>
-  )
+  );
 }
